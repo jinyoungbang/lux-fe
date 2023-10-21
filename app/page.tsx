aab2c85dabddfa557984c007f4e6747ee2ec8951
Taesung Yoon <yoontsy2000@gmail.com>
2023-10-20 20:50:42 -0400

    Created tabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import TransactionList from "@/components/TransactionList"
+
 export default function Home() {
     return (
         <main>
             <h1>Lux, your next financial asset manager</h1>
+            <TransactionList/>
         </main>
     )
 }
