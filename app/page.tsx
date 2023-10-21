84014e40b19c7a4429fe6425ee3442e8ca9892ca
Jin Young Bang <jybang@bu.edu>
2023-10-20 21:48:34 -0400

    push

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,73 @@
-import TransactionList from "@/components/TransactionList"
+"use client"
+
+import { useEffect, useState, useCallback } from "react";
+import TransactionList from "@/components/TransactionList";
+import {
+  usePlaidLink,
+  PlaidLinkOptions,
+} from 'react-plaid-link';
+
 
 export default function Home() {
-    return (
-        <main>
-            <h1>Lux, your next financial asset manager</h1>
-            <TransactionList/>
-        </main>
-    )
+  const [linkToken, setLinkToken] = useState(null);
+
+  const onSuccess = useCallback((public_token: string, metadata: any) => {
+    // Send public_token to the server
+    console.log(public_token)
+    const formData = new FormData();
+    formData.append('public_token', public_token);
+
+    fetch('http://127.0.0.1:5000/api/plaid/exchange_public_token', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => {
+        // Handle the response here
+      })
+      .catch((error) => {
+        console.error('Error sending public_token:', error);
+      });
+  }, []);
+
+  const config: PlaidLinkOptions = {
+    onSuccess: onSuccess,
+    onExit: (err, metadata) => {
+      // Handle exit
+    },
+    onEvent: (eventName, metadata) => {
+      // Handle events
+    },
+    token: linkToken,
+    // required for OAuth; if not using OAuth, set to null or omit:
+    // receivedRedirectUri: window.location.href,
+  };
+
+  const { open, ready } = usePlaidLink(config);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/api/plaid/create_link_token', {
+      method: 'POST',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setLinkToken(data.link_token);
+      })
+      .catch((error) => {
+        console.error('Error fetching Link Token:', error);
+      });
+
+    if (ready) {
+      open();
+    }
+  }, [ready]);
+
+  return (
+    <main>
+      <button className="button is-link" onClick={() => open()} disabled={!ready}>
+        Link account
+      </button>
+      <h1>Lux, your next financial asset manager</h1>
+      <TransactionList />
+    </main>
+  );
 }
